8f13580a9060afc371cd1bd58d4f5552abdc440e
Júlio Ballot <julio.ballot@gmail.com>
2021-02-03 07:18:44 -0300

    Adding roles directory for our Ansible environment.
    At the moment I am only using crio for CRI-O installation, extrapkgs for basic packages and dependencies and kubernetes for kubectl, kubectl and kubeadm.

---
+++ roles/crio/files/install-crio.sh
@@ -0,0 +1,9 @@
+export OS="Debian_Testing"
+export VERSION="1.19"
+
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
+
+curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | apt-key add -
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | apt-key add -
+
